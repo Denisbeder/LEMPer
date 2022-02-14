ff30d1667dfa66d6ff7c44a6203e35fb9dabfdbd
Denisbeder <denisbeder@gmail.com>
2022-02-14 16:14:40 -0400

    rename user supervisor

--- scripts/install_supervisor.sh
+++ scripts/install_supervisor.sh
@@ -11,6 +11,9 @@ echo -e "${CYAN}Optimizing Supervisor...${NC}"
 # Modify the ./supervisor.conf file to match your App's requirements.
 cp -f "etc/supervisor/laravel-worker.conf" "/etc/supervisor/conf.d/laravel-worker.conf"
 
+# Adjust worker.
+sed -i "s/user=user/user=${USERNAME}/g" /etc/supervisor/conf.d/laravel-worker.conf
+
 echo -e "${CYAN}Start Supervisor...${NC}"
 /etc/init.d/supervisor start
 /etc/init.d/supervisor reread
