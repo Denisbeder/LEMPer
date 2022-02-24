670ce9ec94e8d39bc11e605f59b6cb5d3eaf9a89
denisbeder <denisbeder@gmail.com>
2022-02-24 16:31:49 -0400

    Add CLI for create new domains to vhost of NGINX

---
+++ cli.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+SITES_AVALIABLE="etc/nginx/sites-avaliable"
+SITES_ENABLED="etc/nginx/sites-enabled"
+ROOT_PATH_BASE="usr/share/nginx/html"
+VHOST="/etc/nginx/vhost/site_laravel.conf"
+
+printer() {
+	echo "$@"
+}
+
+echo -n "Enter the name a domain: "
+read DOMAIN
+
+echo -n "Enter the root path for NGINX (/$ROOT_PATH_BASE/$DOMAIN/...): "
+read ROOT_PATH
+
+echo -n "User page-cache (yes/no): "
+read USE_PAGE_CACHE
+
+DOMAIN_PATH="$ROOT_PATH_BASE/$DOMAIN"
+
+if [[ "$USE_PAGE_CACHE" == "yes" ]]; then
+	VHOST="/etc/nginx/vhost/site_laravel_page_cache.conf"
+fi
+
+# Confirm directory exists
+if [[ ! -d $SITES_AVALIABLE ]]; then
+    printer "Creating directory $SITES_AVALIABLE"
+	mkdir -p $SITES_AVALIABLE
+fi
+
+# Confirm directory exists
+if [[ ! -d $SITES_ENABLED ]]; then
+    printer "Creating directory $SITES_ENABLED"
+	mkdir -p $SITES_ENABLED
+fi
+
+# Confirm directory exists
+if [[ ! -d "$DOMAIN_PATH/$ROOT_PATH" ]]; then
+    printer "Creating directory $DOMAIN_PATH/$ROOT_PATH"
+	mkdir -p "$DOMAIN_PATH/$ROOT_PATH"
+fi
+
+# Create server to domain the on NGINX
+echo "server {
+    listen 80;
+    listen [::]:80;
+
+    server_name $DOMAIN;
+
+    #include /etc/nginx/includes/ssl.conf;
+    
+    # SSL by Cloudflare
+    #ssl_certificate /etc/cloudflare/publkey.pem;
+    #ssl_certificate_key /etc/cloudflare/privkey.pem;
+    #ssl_client_certificate /etc/cloudflare/origin-pull-ca.pem;
+
+    access_log /var/log/nginx/$DOMAIN.access.log;
+    error_log /var/log/nginx/$DOMAIN.error.log;
+
+    root /usr/share/nginx/html/$DOMAIN/$ROOT_PATH;
+    index index.php index.html index.htm;
+
+    include /etc/nginx/includes/rules_security.conf;
+    include /etc/nginx/includes/rules_staticfiles.conf;
+    include /etc/nginx/includes/rules_restriction.conf;
+
+    include $VHOST;
+
+    location ~ \.php$ {
+        try_files $uri =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_index index.php;
+        include /etc/nginx/fastcgi_params;
+        include /etc/nginx/includes/fastcgi.conf;
+        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+    }
+
+    #include /etc/nginx/includes/error_pages.conf;
+
+    include /etc/nginx/includes/fcgiwrap.conf;
+}" > $SITES_AVALIABLE/$DOMAIN
+
+# Create link simbolic to domain
+#ln -s "$SITES_AVALIABLE/$DOMAIN" "$SITES_ENABLED/$DOMAIN" 
+
+# After set configs restart NGINX
+#systemctl restart nginx.service
+
+printer Domain name $DOMAIN created on $ROOT_PATH
