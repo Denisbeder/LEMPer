a193c0b6c8f903b1f4cd41d104cc465ede478973
denisbeder <denisbeder@gmail.com>
2022-02-26 09:30:47 -0400

    fix install composer

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -98,21 +98,10 @@ fi
 
 # Install Composer
 echo -e "${CYAN}Installing Composer...${NC}"
-EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-echo $RESULT
+curl -sS https://getcomposer.org/installer | php
+chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer
+composer self-update 
 
 # Restart PHP-fpm server.
 if [[ $(pgrep -c "php-fpm${PHPv}") -gt 0 ]]; then
