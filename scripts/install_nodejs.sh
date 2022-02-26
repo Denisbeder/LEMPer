b5d93938e79dbfa4d8cf43a35b71b1b02c95a249
denisbeder <denisbeder@gmail.com>
2022-02-26 19:49:14 -0400

    change installation nodejs

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-### Supervisor installation ###
+### NodeJS installation ###
 echo -e "${CYAN}[NodeJS Installation]${NC}"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source $HOME/.bashrc
+nvm install --lts
+nvm install node
