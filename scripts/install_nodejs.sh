c7dd1629a465cf837aaa5ead9cf03ecea2d887c9
denisbeder <denisbeder@gmail.com>
2022-02-26 20:25:45 -0400

    reconfig instalation nodejs

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-### NodeJS installation ###
+### Supervisor installation ###
 echo -e "${CYAN}[NodeJS Installation]${NC}"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source $HOME/.bashrc
-nvm install --lts
-nvm install node
+# Install the NodeJS
+curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | bash -
+apt update && apt install nodejs
+
+# Install the Yarn package manager
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt update && apt install yarn 
+
+apt autoremove -yqq
